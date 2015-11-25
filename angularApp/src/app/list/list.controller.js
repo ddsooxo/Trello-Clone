79c47195372e10b2b293efa6f137daa15efbcbde
ddsooxo <dchuteng@gmail.com>
2015-11-25 13:08:16 -0800

    killed a couple bugs in auth

--- angularApp/src/app/list/list.controller.js
+++ angularApp/src/app/list/list.controller.js
@@ -80,18 +80,18 @@
 //   'use strict';
 
 //   angular.module('mytodo')
-//     .controller('ListController', function ($scope, $routeParams, $http) {
-      
-//       $scope.formData = {};
-//       $scope.lists = [];
-//       $scope.boardId = $routeParams.board_id;
-//       $scope.board_title = $routeParams.board_title;
-//       console.log($scope.board_title);
+//     .controller('ListController', function (vm, $routeParams, $http) {
+//       var vm  = this; 
+//       vm.formData = {};
+//       vm.lists = [];
+//       vm.boardId = $routeParams.board_id;
+//       vm.board_title = $routeParams.board_title;
+//       console.log(vm.board_title);
       
 //       //show lists
 //       $http.get('/api/lists?board_id=' + $routeParams.board_id)
 //          .success(function(data) {
-//              $scope.lists = data;
+//              vm.lists = data;
 //              console.log(data);
 //          })
 //          .error(function(data) {
@@ -99,12 +99,12 @@
 //          });
 
 //       // create list
-//       $scope.createList = function () {
-//         $scope.formData.board_id = $scope.boardId;
-//          console.log('blah', $scope.formData);
-//         $http.post('/api/list/create', $scope.formData)
+//       vm.createList = function () {
+//         vm.formData.board_id = vm.boardId;
+//          console.log('blah', vm.formData);
+//         $http.post('/api/list/create', vm.formData)
 //            .success(function(data) {
-//                $scope.lists = data;
+//                vm.lists = data;
 //                console.log(data);
 //            })
 //            .error(function(data) {
@@ -113,10 +113,10 @@
 //       };
         
 //       //delete list
-//       $scope.removeList = function (id) {
+//       vm.removeList = function (id) {
 //         $http.post('/api/list/delete/' + id)
 //            .success(function(data) {
-//                $scope.lists = data;
+//                vm.lists = data;
 //                console.log(data);
 //            })
 //            .error(function(data) {
@@ -125,8 +125,8 @@
 //       };
         
 //       //update list
-//       $scope.editList = function (id, list_title) {
-//         $http.post('/api/list/edit/' + id + '?list_title=' + list_title + '&board_id=' + $scope.boardId)
+//       vm.editList = function (id, list_title) {
+//         $http.post('/api/list/edit/' + id + '?list_title=' + list_title + '&board_id=' + vm.boardId)
 //            .error(function(data) {
 //                console.log('Error: ' + data);
 //            });
